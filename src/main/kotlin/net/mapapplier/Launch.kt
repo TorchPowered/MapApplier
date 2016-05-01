0c10a7854376464fa8b558c0101958ab47630d15
PizzaCrust <torchprojectserver@gmail.com>
2016-05-01 16:32:07 +0700

    launch file

---
+++ src/main/kotlin/net/mapapplier/Launch.kt
@@ -0,0 +1,18 @@
+package net.mapapplier
+
+/**
+ * The entry point of the application.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+class Launch {
+    companion object Static {
+        @JvmStatic fun main(args: Array<String>) {
+            // java -jar MapApplier.jar -obfmap:example.obfmap -jar
+            /*
+                TODO
+             */
+        }
+    }
+}
